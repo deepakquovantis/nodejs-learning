47cd6bebe95f65a62ac6b53602eeb794ac7b6a2c
deepak <deepak.singh@quovantis.com>
2020-05-27 14:12:20 +0530

    tic toc toe game

---
+++ assignment-1.js
@@ -0,0 +1,68 @@
+//TODO:// the following conditions can be further added
+// 1- Game over
+// 2- Game level
+// 3- Play with computer
+
+let arr =
+    [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ];
+
+function checkStatus(travese) {
+    for (let i = 0; i < arr.length; i++) {
+        let prev = undefined;
+        let count = 0;
+        for (let j = 0; j < arr[i].length; j++) {
+
+            if (travese == 'vertically') {
+                if (prev === undefined) {
+                    prev = arr[i][j];
+                    count = 1;
+                }
+                if (j > 0 && arr[i][j] == prev) {
+                    prev = arr[i][j];
+                    count++;
+                }
+            }
+            if (travese == 'horizontally') {
+                if (prev === undefined) {
+                    prev = arr[j][i];
+                    count = 1;
+                }
+
+                if (j > 0 && arr[j][i] == prev) {
+                    prev = arr[j][i];
+                    count++;
+                }
+            }
+
+            //TODO:// This can be done more dianamically
+            if (travese == 'diagonally') {
+                if (i == j && j == 2 && arr[i - 1][j - 1] == arr[0][0] && arr[i][j] == arr[0][0]) {
+                    count = 3;
+                }
+                else if (i == j && j == 2 && arr[0][j] == arr[0][0] && arr[0][j] == arr[1][1]) {
+                    count = 3;
+                }
+            }
+        }
+        if (count == 3) {
+            return { isSuccess: true, fromIndex: i, traveseDirection: travese };
+        }
+    }
+}
+
+
+function play() {
+    let travese = ['vertically', 'horizontally', 'diagonally'];
+    travese.forEach(element => {
+        let status = checkStatus(element);
+        if (status.isSuccess) {
+            console.log('Game completed ' + status.traveseDirection + ' from index ' + status.fromIndex);
+        }
+    });
+}
+
+play();
---
+++ assignment-2.js
@@ -0,0 +1,79 @@
+//TODO:// the following conditions can be further added
+// 1- Game over
+// 2- Game level
+// 3- Play with computer
+
+class baseGame {
+    constructor() {
+    }
+    play() { }
+}
+
+class TicTacToe extends baseGame {
+    constructor() {
+        super()
+    }
+    arr =
+        [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1]
+        ];
+
+    checkStatus(travese) {
+        for (let i = 0; i < this.arr.length; i++) {
+            let prev = undefined;
+            let count = 0;
+            for (let j = 0; j < this.arr[i].length; j++) {
+
+                if (travese == 'vertically') {
+                    if (prev === undefined) {
+                        prev = this.arr[i][j];
+                        count = 1;
+                    }
+                    if (j > 0 && this.arr[i][j] == prev) {
+                        prev = this.arr[i][j];
+                        count++;
+                    }
+                }
+                if (travese == 'horizontally') {
+                    if (prev === undefined) {
+                        prev = this.arr[j][i];
+                        count = 1;
+                    }
+
+                    if (j > 0 && this.arr[j][i] == prev) {
+                        prev = this.arr[j][i];
+                        count++;
+                    }
+                }
+
+                //TODO:// This can be done more dianamically
+                if (travese == 'diagonally') {
+                    if (i == j && j == 2 && this.arr[i - 1][j - 1] == this.arr[0][0] && this.arr[i][j] == this.arr[0][0]) {
+                        count = 3;
+                    }
+                    else if (i == j && j == 2 && this.arr[0][j] == this.arr[0][0] && this.arr[0][j] == this.arr[1][1]) {
+                        count = 3;
+                    }
+                }
+            }
+            if (count == 3) {
+                return { isSuccess: true, fromIndex: i, traveseDirection: travese };
+            }
+        }
+    }
+
+    play() {
+        let travese = ['vertically', 'horizontally', 'diagonally'];
+        travese.forEach(element => {
+            let status = this.checkStatus(element);
+            if (status.isSuccess) {
+                console.log('Game completed ' + status.traveseDirection + ' from index ' + status.fromIndex);
+            }
+        });
+    }
+}
+
+let ticTacToe = new TicTacToe();
+ticTacToe.play();
